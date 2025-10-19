97928353b4e2d6eba924dad6d8b2e6c5011f075a
Mark <margar.melkonyan@bk.ru>
2025-10-19 17:57:23 +0300

    fix: logger middleware

--- internal/platform/http/middleware/logger.go
+++ internal/platform/http/middleware/logger.go
@@ -2,16 +2,24 @@ package middleware
 
 import (
 	"log/slog"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func LogRequest(c *fiber.Ctx) error {
-	slog.Info("Request received",
+	start := time.Now()
+
+	c.Next()
+
+	slog.Info("Request completed",
 		slog.String("method", c.Method()),
 		slog.String("path", c.Path()),
 		slog.String("ip", c.IP()),
 		slog.String("user-agent", string(c.Request().Header.UserAgent())),
+		slog.Int("status-code", c.Response().StatusCode()),
+		slog.Duration("duration", time.Since(start)),
 	)
-	return c.Next()
+
+	return nil
 }
