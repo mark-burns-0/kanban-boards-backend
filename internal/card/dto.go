127444f13c57f325b61180a9b696c071955eb7bf
Mark <margar.melkonyan@bk.ru>
2025-10-24 22:48:42 +0300

    refactor: card/dto

--- internal/card/dto.go
+++ internal/card/dto.go
@@ -1,53 +1,32 @@
 package card
 
 import (
-	"database/sql/driver"
-	"encoding/json"
-	"errors"
 	"time"
 )
-
-type cardProperties map[string]string
-
-func (cp cardProperties) Value() (driver.Value, error) {
-	if cp == nil {
-		return nil, nil
-	}
-	return json.Marshal(cp)
-}
-
-func (cp *cardProperties) Scan(value any) error {
-	if value == nil {
-		*cp = nil
-		return nil
-	}
-
-	bytes, ok := value.([]byte)
-	if !ok {
-		return errors.New("type assertion to []byte failed")
-	}
-
-	return json.Unmarshal(bytes, cp)
-}
 
 type Card struct {
 	ID          uint64
 	BoardID     uint64
 	ColumnID    uint64
+	Position    uint64
 	Text        string
 	Description string
-	Position    uint64
-	AssignedTo  uint64
-	Properties  cardProperties
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   *time.Time
+	cardProperties
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
 }
 
 type CardRequest struct {
-	BoardID     uint64 `json:"board_id" validate:"required,min=1"`
-	ColumnID    uint64 `json:"column_id" validate:"required,min=1"`
-	Text        string `json:"text" validate:"required,min=1,max=255"`
-	Description string `json:"description" validate:"required,min=1,max=255"`
-	Position    uint64 `json:"position" validate:"required,min=1"`
+	BoardID        uint64 `json:"board_id" validate:"required,min=1"`
+	ColumnID       uint64 `json:"column_id" validate:"required,min=1"`
+	Position       uint64 `json:"position" validate:"required,min=1"`
+	Text           string `json:"text" validate:"required,min=1,max=255"`
+	Description    string `json:"description" validate:"required,min=1,max=255"`
+	cardProperties `json:"properties"`
+}
+
+type cardProperties struct {
+	Color string `json:"color,omitempty" validate:"min=1,max=255"`
+	Tag   string `json:"tag,omitempty" validate:"min=1,max=255"`
 }
