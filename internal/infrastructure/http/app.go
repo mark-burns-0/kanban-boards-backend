937a0cb512592545ac08d2f132ca98475001dc7b
Mark <margar.melkonyan@bk.ru>
2025-11-03 15:13:41 +0300

    feat: turn on prefork mode

--- internal/infrastructure/http/app.go
+++ internal/infrastructure/http/app.go
@@ -1,7 +1,13 @@
 package http
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+)
+
+const (
+	timeout = 5 * time.Second
 )
 
 type Config interface {
@@ -10,7 +16,10 @@ type Config interface {
 
 func NewApp(cfg Config) *fiber.App {
 	app := fiber.New(fiber.Config{
-		AppName: cfg.GetAppName(),
+		Prefork:      true,
+		ReadTimeout:  timeout,
+		WriteTimeout: timeout,
+		AppName:      cfg.GetAppName(),
 	})
 	return app
 }
