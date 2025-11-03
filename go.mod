171d548879e0d2ae2dc036ada81678e3649504ad
Mark M <>
2025-11-03 18:29:01 +0300

    feat: add test for car mapper

--- go.mod
+++ go.mod
@@ -9,13 +9,14 @@ require (
 	github.com/iancoleman/strcase v0.3.0
 	github.com/jackc/pgx/v5 v5.7.6
 	github.com/joho/godotenv v1.5.1
-	github.com/k0kubun/pp v2.4.0+incompatible
+	github.com/stretchr/testify v1.8.4
 	golang.org/x/crypto v0.43.0
 	golang.org/x/sync v0.17.0
 )
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -23,17 +24,20 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/net v0.45.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ internal/card/transport/mapper_test.go
@@ -0,0 +1,246 @@
+package transport
+
+import (
+	"backend/internal/card/domain"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ToCardMoveCommand(t *testing.T) {
+	mapper := CardMapper{}
+	tests := []struct {
+		name     string
+		req      *CardMoveRequest
+		expected *domain.CardMoveCommand
+	}{
+		{
+			name: "zero values",
+			req: &CardMoveRequest{
+				ID:           0,
+				FromColumnID: 0,
+				ToColumnID:   0,
+				FromPosition: 0,
+				ToPosition:   0,
+				BoardID:      "",
+			},
+			expected: &domain.CardMoveCommand{
+				ID:           0,
+				FromColumnID: 0,
+				ToColumnID:   0,
+				FromPosition: 0,
+				ToPosition:   0,
+				BoardID:      "",
+			},
+		},
+		{
+			name: "empty board ID",
+			req: &CardMoveRequest{
+				ID:           1,
+				FromColumnID: 2,
+				ToColumnID:   3,
+				FromPosition: 4,
+				ToPosition:   5,
+				BoardID:      "",
+			},
+			expected: &domain.CardMoveCommand{
+				ID:           1,
+				FromColumnID: 2,
+				ToColumnID:   3,
+				FromPosition: 4,
+				ToPosition:   5,
+				BoardID:      "",
+			},
+		},
+		{
+			name: "valid data",
+			req: &CardMoveRequest{
+				ID:           1,
+				FromColumnID: 2,
+				ToColumnID:   3,
+				FromPosition: 4,
+				ToPosition:   5,
+				BoardID:      "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+			},
+			expected: &domain.CardMoveCommand{
+				ID:           1,
+				FromColumnID: 2,
+				ToColumnID:   3,
+				FromPosition: 4,
+				ToPosition:   5,
+				BoardID:      "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+			},
+		},
+		{
+			name:     "nil pointer check",
+			req:      nil,
+			expected: nil,
+		},
+	}
+
+	for _, tc := range tests {
+		name := fmt.Sprintf("case (%s)", tc.name)
+		t.Run(name, func(t *testing.T) {
+			actual := mapper.ToCardMoveCommand(tc.req)
+			assert.Equal(t, actual, tc.expected)
+		})
+	}
+}
+func Test_ToCard(t *testing.T) {
+	mapper := CardMapper{}
+	var color, tag *string
+	color = new(string)
+	tag = new(string)
+	*color = "color"
+	*tag = "tag"
+
+	tests := []struct {
+		name     string
+		req      *CardRequest
+		expected *domain.Card
+	}{
+		{
+			name:     "nil pointer",
+			req:      nil,
+			expected: nil,
+		},
+		{
+			name: "main information",
+			req: &CardRequest{
+				ID:          1,
+				ColumnID:    1,
+				BoardID:     "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:        "test text",
+				Description: "test description",
+			},
+			expected: &domain.Card{
+				ID:          1,
+				ColumnID:    1,
+				BoardID:     "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:        "test text",
+				Description: "test description",
+			},
+		},
+		{
+			name: "with color and tag properties",
+			req: &CardRequest{
+				ID:          1,
+				ColumnID:    1,
+				BoardID:     "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:        "test text",
+				Description: "test description",
+				cardProperties: &cardProperties{
+					Color: color,
+					Tag:   tag,
+				},
+			},
+			expected: &domain.Card{
+				ID:          1,
+				ColumnID:    1,
+				BoardID:     "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:        "test text",
+				Description: "test description",
+				CardProperties: domain.CardProperties{
+					Color: "color",
+					Tag:   "tag",
+				},
+			},
+		},
+		{
+			name: "with color property only",
+			req: &CardRequest{
+				ID:          1,
+				ColumnID:    1,
+				BoardID:     "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:        "test text",
+				Description: "test description",
+				cardProperties: &cardProperties{
+					Color: color,
+				},
+			},
+			expected: &domain.Card{
+				ID:          1,
+				ColumnID:    1,
+				BoardID:     "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:        "test text",
+				Description: "test description",
+				CardProperties: domain.CardProperties{
+					Color: "color",
+				},
+			},
+		},
+		{
+			name: "with tag property only",
+			req: &CardRequest{
+				ID:          1,
+				ColumnID:    1,
+				BoardID:     "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:        "test text",
+				Description: "test description",
+				cardProperties: &cardProperties{
+					Tag: tag,
+				},
+			},
+			expected: &domain.Card{
+				ID:          1,
+				ColumnID:    1,
+				BoardID:     "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:        "test text",
+				Description: "test description",
+				CardProperties: domain.CardProperties{
+					Tag: "tag",
+				},
+			},
+		},
+		{
+			name: "with nil properties pointer",
+			req: &CardRequest{
+				ID:             1,
+				ColumnID:       1,
+				BoardID:        "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:           "test text",
+				Description:    "test description",
+				cardProperties: &cardProperties{},
+			},
+			expected: &domain.Card{
+				ID:             1,
+				ColumnID:       1,
+				BoardID:        "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:           "test text",
+				Description:    "test description",
+				CardProperties: domain.CardProperties{},
+			},
+		},
+		{
+			name: "with nil property values",
+			req: &CardRequest{
+				ID:          1,
+				ColumnID:    1,
+				BoardID:     "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:        "test text",
+				Description: "test description",
+				cardProperties: &cardProperties{
+					Color: nil,
+					Tag:   nil,
+				},
+			},
+			expected: &domain.Card{
+				ID:             1,
+				ColumnID:       1,
+				BoardID:        "e102c99e-651c-44e1-bff1-c4a22e3134ce",
+				Text:           "test text",
+				Description:    "test description",
+				CardProperties: domain.CardProperties{},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		name := fmt.Sprintf("case (%s)", tc.name)
+		t.Run(name, func(t *testing.T) {
+			actual := mapper.ToCard(tc.req)
+			assert.Equal(t, actual, tc.expected)
+		})
+	}
+}
